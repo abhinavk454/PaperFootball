bee4ef00115f72099c8b1698c9749aaa57410c1e
Abhinav Kumar <abhinav.vikash2@gmail.com>
2023-02-02 13:06:36 +0530

    Intro Screen V0.0.1

--- intro.gd
+++ intro.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$Player.play("Fade In")
+	await get_tree().create_timer(6).timeout
+	$Player.play("Fade Out")
+	await get_tree().create_timer(5).timeout
+	# TODO : Replace below with scene
+	get_tree().quit()

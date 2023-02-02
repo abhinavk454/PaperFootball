a7e6c6f7031aca3c6fb6d1a113de4ff566f00f64
Abhinav <124233671+abhinavat2nd@users.noreply.github.com>
2023-02-02 22:55:43 +0530

    Created Start Screen
    For Version 0.0.1

---
+++ start.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$StartButton.connect("pressed",_on_touched)
+	$ExitButton.connect("pressed",_on_touched)
+	
+func _on_touched():
+	get_tree().quit()
